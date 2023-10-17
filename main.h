e768ba3565f7c034f253dd35c9fbf6689b476ed3
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-10-17 16:42:49 -0700

    updated

--- main.h
+++ main.h
@@ -6,6 +6,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+typedef struct format
+{
+	char *pd;
+	int (*fn)();
+
+} match;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_c(va_list val);
@@ -14,8 +23,8 @@ int _strlen(char *str);
 int print_37(void);
 int _decimal(va_list args);
 int _interger(va_list args);
-int _binary(va_list val)
-
+int _binary(va_list val);
+int printf_s(va_list val);
 
 
 
