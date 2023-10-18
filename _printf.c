558e49750c64aa437df17214f26e4ed35e9cf85a
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-10-17 17:40:20 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -4,54 +4,55 @@
  * @format: constant pointer
  * Return: Character input
  */
-
-
+int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 	int print_char = 0;
 	va_list args;
-    
+
 	va_start(args, format);
-	if(*format == ' ')
-        return (1);
-        
-	while(*format)
+	if (*format == ' ')
+	return (1);
+
+	while (*format)
 	{
-	if(*format != '%')
-	{
-		write(1, format, 1);
-		print_char++;
-	}
-	else
-	{
-		format++;
-		if(*format == '\0')
-			break;
-		if(*format == '%')
+		if (*format != '%')
 		{
 			write(1, format, 1);
 			print_char++;
 		}
-		else if(*format == 'c')
+		else
 		{
-			char c = va_arg(args, int);
-			write(1, &c,1);
-			print_char++;
-		}
-		else if(*format == 's')
-		{
-			char *str = va_arg(args, char*);
-			int len = 0;
-			for (len = 0; str[len] != '\0'; str++)
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
 			{
-			write(1, str, len);
-			print_char += len;
+				write(1, format, 1);
+				print_char++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				print_char++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				int len = 0;
+
+				for (len = 0; str[len] != '\0'; str++)
+				{
+					write(1, str, len);
+					print_char += len;
+				}
 			}
 		}
+
+		format++;
 	}
-        
-	format++;    
-}
-	va_end(args);    
+	va_end(args);
 	return (print_char);
 }
