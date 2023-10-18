cdfeb047632eba91f1b65c4093fe87ec90a8eb8a
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-17 17:54:34 -0700

    no file

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
--- print_c.c
+++ print_c.c
@@ -1,14 +1,48 @@
 #include "main.h"
 /**
- * printf_c - Function that prints character
- * @val: argument.
- * Return: 1 (success)
+ * printf_c - A Function that prints a singel character
+ * to the standard output
+ * @format: An argument pointer
+ * Return: Always 0
  */
-int printf_c(va_list val)
+int _printf(const char *format, ...)
 {
-	char str;
+	char print_char = 0;
+	va_list args;
 
-	str = va_arg(val, int);
-	_putchar(str);
-	return (1);
+	if (*format == NULL)
+		return (1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			print_char++;
+		}
+		else
+		{
+			format++; /*check the next chararcter*/
+			if (*format =='\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				print_char++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				write (1, &c, 1);
+				print_char++;
+			}
+			else if (*format == 's')
+			{
+				len = 0;
+				char *str = va_arg(args, char*);
+				for (len = 0; str[len] != '\0'; str++)
+				{
+
+
+	}
 }
