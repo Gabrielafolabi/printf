74b33fe320ac150fcde8b40941879ae8e9d87db5
emmaculate-exceel <emmaculate2exceel@live.com>
2023-10-24 18:26:36 -0700

    update printf

--- _printf.c
+++ _printf.c
@@ -5,54 +5,40 @@
  * Return: Character input
  */
 int _printf(const char *format, ...);
-int _printf(const char *format, ...)
-{
-	int print_char = 0, count = 0;
-	va_list args;
+int _printf(const char *format, ...) {
+    int print_char = 0;
+    va_list args;
+    va_start(args, format);
 
-	va_start(args, format);
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	while (format[count] != '\0')
-	{
-		if (format[count] != '%')
-		{
-			write(1, &format[count], 1);
-			print_char++;
-		}
-		else
-		{
-			format++;
-			if (format[count] == '\0')
-				break;
-			if (format[count] == '%')
-			{
-				write(1, &format[count], 1);
-				print_char++;
-			}
-			else if (format[count] == 'c')
-			{
-				char c = va_arg(args, int);
-
-				write(1, &c, 1);
-				print_char++;
-			}
-			else if (format[count] == 's')
-			{
-				char *str = va_arg(args, char*);
-				int len;
-
-				for (len = 0; str[len] != '\0'; len++)
-				{
-					write(1, &str[len], len);
-					print_char += len;
-				}
-			}
-		}
-
-		count++;
-	}
-	va_end(args);
-	return (print_char);
+    while (*format != '\0') {
+        if (*format != '%') {
+            write(1, format, 1);
+            print_char++;
+        } else {
+            format++;  // Move past the '%'
+            if (*format == '\0') break;
+            if (*format == '%') {
+                write(1, format, 1);
+                print_char++;
+            } else if (*format == 'c') {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                print_char++;
+            } else if (*format == 's') {
+                char *str = va_arg(args, char*);
+                while (*str) {
+                    write(1, str, 1);
+                    print_char++;
+                    str++;
+                }
+            }
+        }
+        format++;
+    }
+    va_end(args);
+    return print_char;
 }
+
